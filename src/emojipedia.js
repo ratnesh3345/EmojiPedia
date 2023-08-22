4aeafb0c0fc8b81bed6daf9e4aec74175e4be96c
ratnesh3345 <ratnesh3345@gmail.com>
2023-08-22 21:58:54 +0530

    EmojiPedia

---
+++ src/emojipedia.js
@@ -0,0 +1,150 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+
+  {
+    id: 4,
+    emoji: "😄",
+    name: "Smiling Face with Open Mouth and Smiling Eyes",
+    meaning:
+      "Happiness or excitement. A happy, smiling face with smiling eyes and open mouth."
+  },
+  {
+    id: 5,
+    emoji: "🌸",
+    name: "Cherry Blossom",
+    meaning:
+      "A pink flower often associated with spring. Represents beauty, fragility, and the fleeting nature of life."
+  },
+  // Add more emojis and their descriptions below
+  {
+    id: 6,
+    emoji: "🐶",
+    name: "Dog Face",
+    meaning:
+      "A cute dog face. Represents loyalty, companionship, and affection."
+  },
+  {
+    id: 7,
+    emoji: "🌍",
+    name: "Earth Globe Europe-Africa",
+    meaning:
+      "A representation of the Earth, focusing on Europe and Africa. Symbolizes the planet and its continents."
+  },
+  {
+    id: 8,
+    emoji: "🍕",
+    name: "Pizza",
+    meaning:
+      "A slice of pizza. Represents food, especially fast food, and indulgence."
+  },
+  {
+    id: 9,
+    emoji: "🚀",
+    name: "Rocket",
+    meaning:
+      "A rocket launching into space. Symbolizes exploration, innovation, and progress."
+  },
+  {
+    id: 10,
+    emoji: "🎉",
+    name: "Party Popper",
+    meaning:
+      "A party popper, often used to celebrate special occasions. Represents excitement and joy."
+  },
+  {
+    id: 11,
+    emoji: "🌞",
+    name: "Sun with Face",
+    meaning:
+      "A bright sun with a friendly face. Represents warmth, happiness, and sunny weather."
+  },
+  {
+    id: 12,
+    emoji: "🌈",
+    name: "Rainbow",
+    meaning:
+      "A colorful arc in the sky. Symbolizes hope, diversity, and beauty."
+  },
+  {
+    id: 13,
+    emoji: "🎮",
+    name: "Video Game Controller",
+    meaning:
+      "A handheld video game controller. Represents gaming, entertainment, and technology."
+  },
+  {
+    id: 14,
+    emoji: "📚",
+    name: "Books",
+    meaning: "A stack of books. Represents learning, knowledge, and reading."
+  },
+  {
+    id: 15,
+    emoji: "🚀",
+    name: "Rocket",
+    meaning:
+      "A rocket blasting off into space. Symbolizes progress, ambition, and exploration."
+  },
+  {
+    id: 16,
+    emoji: "🎉",
+    name: "Party Popper",
+    meaning:
+      "A party popper releasing confetti. Represents celebration, excitement, and joy."
+  },
+  {
+    id: 17,
+    emoji: "🍔",
+    name: "Hamburger",
+    meaning: "A classic hamburger. Represents food, fast food, and indulgence."
+  },
+  {
+    id: 18,
+    emoji: "🌍",
+    name: "Earth Globe Europe-Africa",
+    meaning:
+      "A globe showing Europe and Africa. Represents the Earth, global awareness, and unity."
+  },
+  {
+    id: 19,
+    emoji: "🌺",
+    name: "Hibiscus",
+    meaning:
+      "A beautiful hibiscus flower. Symbolizes beauty, nature, and tropical locations."
+  },
+  {
+    id: 20,
+    emoji: "🌟",
+    name: "Glowing Star",
+    meaning: "A shining star. Represents brilliance, success, and inspiration."
+  },
+  {
+    id: 21,
+    emoji: "🐼",
+    name: "Panda Face",
+    meaning:
+      "A cute and chubby panda face. Often used to symbolize pandas, nature, or being adorable."
+  }
+];
+
+export default emojipedia;
